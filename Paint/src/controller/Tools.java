92ef6bbe96ffe86981eed16d1adc2129b45cb141
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2018-04-17 19:08:34 +0200

    5od commit

--- Paint/src/controller/Tools.java
+++ Paint/src/controller/Tools.java
@@ -5,10 +5,46 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 /**
  *
  * @author Tommy
  */
 public class Tools {
+    private class ItemListenerHandler implements ItemListener
+    {
+        public void itemStateChanged( ItemEvent event )
+        {
+            // process filled checkbox events
+            if ( event.getSource() == filled )
+            {
+                boolean checkFill=filled.isSelected() ? true : false; //
+                panel.setCurrentShapeFilled(checkFill);
+            }
+            
+            // determine whether combo box selected
+            if ( event.getStateChange() == ItemEvent.SELECTED )
+            {
+                //if event source is combo box colors pass in colorArray at index selected.
+                if ( event.getSource() == colors)
+                {
+                    panel.setCurrentShapeColor
+                        (colorArray[colors.getSelectedIndex()]);
+                }
+                
+                //else if event source is combo box shapes pass in index selected
+                else if ( event.getSource() == shapes)
+                {
+                    panel.setCurrentShapeType(shapes.getSelectedIndex());
+                }
+            }
+            
+        } // end method itemStateChanged
+    }
+    
     
 }
